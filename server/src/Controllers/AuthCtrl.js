bbac5e15d877cadc29adf4dfdb4767867d2374d3
phucB1906745 <87224628+EnzoMrPhuc@users.noreply.github.com>
2022-11-01 16:03:38 +0700

    Add basic logic for login and register Ctrl

--- server/src/Controllers/AuthCtrl.js
+++ server/src/Controllers/AuthCtrl.js
@@ -1,20 +1,63 @@
+import UserModel from '../Models/User.js';
+import bcrypt from 'bcrypt';
+
 class AuthCtrl {
-   // [GET] ...api/auth/login
-   login(req, res) {
-      res.send('This is login page');
+   // [GET] .../api/auth/login
+   async login(req, res) {
+      try {
+         const user = await UserModel.findOne({ username: req.body.username });
+         if (user) {
+            // Check password
+            const match = bcrypt.compare(req.body.password, user.password);
+            if (match) {
+               // Create token and store refresh_token in cookie
+
+               // Handle user for return
+
+               return res.status(200).json({
+                  message: 'Login Success',
+                  user,
+                  accessToken: 'This is token for you',
+               });
+            } else {
+               return res.status(403).json({ message: 'Incorrect password' });
+            }
+         } else {
+            return res.status(400).json({ message: 'This username does not exist' });
+         }
+      } catch (error) {
+         return res.status(500).json({ message: error.message, error });
+      }
    }
 
-   // [GET] ...api/auth/logout
+   // [GET] .../api/auth/logout
    logout(req, res) {
-      res.send('This is login page');
+      try {
+      } catch (error) {}
    }
 
-   // [POST] ...api/auth/register
-   register(req, res) {
-      res.send('This is login page');
+   // [POST] .../api/auth/register
+   async register(req, res) {
+      try {
+         const isUser = await UserModel.findOne({ username: req.body.username });
+         if (isUser) {
+            return res.status(400).json({ message: 'This username already exists' });
+         } else {
+            // Hash password
+            const hasPassword = await bcrypt.hash(req.body.password, 10);
+            // Store in DB
+            const dataUser = { ...req.body, password: hasPassword };
+            const newUser = new UserModel(dataUser);
+            await newUser.save();
+            // Return
+            return res.status(200).json({ message: 'Register Success', user: newUser });
+         }
+      } catch (error) {
+         return res.status(500).json({ message: error.message, error });
+      }
    }
 
-   // [GET] ...api/auth/refreshtoken
+   // [GET] .../api/auth/refreshtoken
    refresh_token(req, res) {
       res.send('This is login page');
    }
--- server/src/Models/User.js
+++ server/src/Models/User.js
@@ -1,2 +1,29 @@
 import mongoose from 'mongoose';
 
+const Schema = mongoose.Schema;
+
+const UserModel = new Schema(
+   {
+      firstname: { type: String, required: true, trim: true },
+      lastname: { type: String, required: true, trim: true },
+      age: { type: Number, min: 13 },
+      username: { type: String, required: true, trim: true, maxlength: 25, unique: true },
+      password: { type: String, required: true },
+      avatar: {
+         type: String,
+         default:
+            'https://images.unsplash.com/photo-1608889335941-32ac5f2041b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+      },
+      role: { type: String, default: 'user' },
+      email: { type: String, required: true, trim: true, unique: true },
+      gender: { type: String, default: 'male   ' },
+      phone: { type: String, default: '' },
+      address: { type: String, default: '' },
+      story: { type: String, default: '', maxlength: 200 },
+      followers: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
+      following: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
+   },
+   { timestamps: true }
+);
+
+export default mongoose.model('User', UserModel);
